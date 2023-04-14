b5ea4e6792cc0f28644b6543528994a9b1d7e86f
Agustin Young <aguyoung@gmail.com>
2023-04-14 18:06:07 -0300

    nested post comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :post_categories
   resources :categories
-  resources :comments
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
